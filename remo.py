f51b1ee866eba1ae4c9b4f1b9dfd39daba695dc4
Guilherme-silvestri <64138274+Guilherme-silvestri@users.noreply.github.com>
2020-05-12 08:34:01 -0300

    Delete remo.py

--- remo.py
+++
@@ -1,5 +0,0 @@
-import os
-phpDep = ["php72-php","php72-php-common","php72-php-bz2","php72-php-curl","php72-php-ldap","php72-php-gd","php72-php-gmp","php72-php-imap","php72-php-mbstring","php72-php-mcrypt","php72-php-soap"]
-for a in phpDep:
-	os.system("yum remove -y " + a)
-
